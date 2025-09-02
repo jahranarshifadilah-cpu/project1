a5f8bf90e9c3406542177e43b2ebc84dae1a7600
jahranarshifadilah-cpu <jahranarshifadilah@gmail.com>
2025-09-02 08:10:10 +0700

    Update soal1.php

---
+++ soal1.php
@@ -0,0 +1,13 @@
+<?php
+$nilai = 65;
+
+if (nilai >=80) {
+    echo "lulus dengan predikat A";
+}else if (nilai >= 75) {
+    echo "lulus dengan predikat B+";
+}elseif (nilai >= 60) {
+        echo "lulus dengan predikat c";
+}else {
+    echo "tidak lulus";
+}
+?>
